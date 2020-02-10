c098d6dbd780e09429747a14930a3b48f743383c
KeitaShiya <keita.shiya@gmail.com>
2020-02-10 15:15:10 +0900

    Use "android.hardware.camera2.CameraManager" instead of "android.hardware.Camera"

--- android/src/main/java/plugins/flutter/lamp/lamp/LampPlugin.java
+++ android/src/main/java/plugins/flutter/lamp/lamp/LampPlugin.java
@@ -1,7 +1,6 @@
 package plugins.flutter.lamp.lamp;
 
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -9,69 +8,80 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import android.os.Build;
+import android.os.Handler;
+
 /**
  * LampPlugin
  */
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class LampPlugin implements MethodCallHandler {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private LampPlugin(Registrar registrar) {
         this._registrar = registrar;
-        this._camera = this.getCamera();
+        cameraManager = (CameraManager) _registrar.context().getSystemService(Context.CAMERA_SERVICE);
+        cameraManager.registerTorchCallback(new CameraManager.TorchCallback() {
+            @Override
+            public void onTorchModeChanged(@NonNull String cameraId, boolean enabled) {
+                super.onTorchModeChanged(cameraId, enabled);
+                cameraID = cameraId;
+            }
+        }, new Handler());
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public static void registerWith(Registrar registrar) {
         final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/clovisnicolas/flutter_lamp");
         channel.setMethodCallHandler(new LampPlugin(registrar));
     }
 
-    private Camera _camera;
     private Registrar _registrar;
+    private CameraManager cameraManager;
+    private String cameraID;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMethodCall(MethodCall call, Result result) {
-        switch(call.method){
-            case "turnOn":
-                this.turn(true);
-                result.success(null);
-                break;
-            case "turnOff":
-                this.turn(false);
-                result.success(null);
-                break;
-            case "hasLamp":
-                result.success(this.hasLamp());
-                break;
-            default:
-                result.notImplemented();
-        }
-    }
-
-    private Camera getCamera() {
         try {
-            return Camera.open();
-        } catch (Exception e) {
-            System.out.println("Failed to get camera : " + e.toString());
-            return null;
+            switch(call.method){
+                case "turnOn":
+                    this.turn(true);
+                    result.success(null);
+                    break;
+                case "turnOff":
+                    this.turn(false);
+                    result.success(null);
+                    break;
+                case "hasLamp":
+                    result.success(this.hasLamp());
+                    break;
+                default:
+                    result.notImplemented();
+            }
+        } catch (CameraAccessException e) {
+            result.error(String.valueOf(e.getReason()), e.getLocalizedMessage(), e.getMessage());
         }
     }
 
-    private void turn(boolean on) {
-        Camera.Parameters params;
-        if (_camera == null || !hasLamp()) {
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void turn(boolean on) throws CameraAccessException {
+        if (!hasLamp()) {
             return;
         }
-        params = _camera.getParameters();
-        params.setFlashMode(on ? Camera.Parameters.FLASH_MODE_TORCH : Camera.Parameters.FLASH_MODE_OFF);
-        _camera.setParameters(params);
-        if (on) {
-            _camera.startPreview();
-        } else {
-            _camera.stopPreview();
-        }
+        if (cameraManager == null || cameraID == null)
+            return ;
+        cameraManager.setTorchMode(cameraID, on);
     }
 
     private boolean hasLamp() {
         return _registrar.context().getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
     }
-
 }
